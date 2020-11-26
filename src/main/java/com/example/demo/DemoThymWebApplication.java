7694df8045021a54971972c5822cdce9730fb7a6
mikesvb <mikesvb@mail.ru>
2020-11-26 10:47:10 +0900

    first

---
+++ src/main/java/com/example/demo/DemoThymWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoThymWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoThymWebApplication.class, args);
+	}
+
+}
